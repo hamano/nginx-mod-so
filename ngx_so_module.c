dd780d4bf7b36d716c364b8599c8ab662ff8101f
Tsukasa Hamano <code@cuspy.org>
2011-12-01 18:15:40 +0900

    deter duplicate loading.

--- ngx_so_module.c
+++ ngx_so_module.c
@@ -48,19 +48,21 @@ static char *
 ngx_so(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     void *handle;
-    ngx_module_t *sym;
+    ngx_module_t *module;
     ngx_str_t *value = cf->args->elts;
     char *symname = (char *)value[1].data;
     char *filename = (char *)value[2].data;
+    int i;
 
+    ngx_conf_log_error(NGX_LOG_STDERR, cf, 0, "loading %s", filename);
     handle = dlopen(filename, RTLD_LAZY);
     if(!handle){
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
                            "dlopen error: %s", dlerror());
         return NGX_CONF_ERROR;
     }
-    sym = dlsym(handle, symname);
-    if(!sym){
+    module = dlsym(handle, symname);
+    if(!module){
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
                            "dlsym error: %s", dlerror());
         return NGX_CONF_ERROR;
@@ -72,9 +74,19 @@ ngx_so(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         return NGX_CONF_ERROR;
     }
 
-    sym->index = ngx_max_module;
-    ngx_modules[ngx_max_module++] = sym;
+    // deter duplicate loading.
+    for(i=0; ngx_modules[i]; i++){
+        if(module == ngx_modules[i]){
+            ngx_conf_log_error(NGX_LOG_WARN, cf, 0,
+                               "already loaded %s", filename);
+            return NGX_CONF_OK;
+        }
+    }
+
+    module->index = ngx_max_module;
+    ngx_modules[ngx_max_module++] = module;
     so_num++;
 
+    ngx_conf_log_error(NGX_LOG_STDERR, cf, 0, "loaded %s", filename);
     return NGX_CONF_OK;
 }
