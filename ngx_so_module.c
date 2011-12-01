8251d7e44622c582e0ce6ffdcc203e49641ba3c7
Tsukasa Hamano <code@cuspy.org>
2011-12-01 20:08:39 +0900

    check abi version

--- ngx_so_module.c
+++ ngx_so_module.c
@@ -4,6 +4,7 @@
 
 #include <dlfcn.h>
 
+#include <nginx.h>
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
@@ -12,7 +13,7 @@ static char *ngx_so(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
 static ngx_command_t  ngx_so_commands[] = {
     { ngx_string("module"),
-      NGX_MAIN_CONF|NGX_CONF_TAKE2,
+      NGX_MAIN_CONF|NGX_CONF_TAKE1,
       ngx_so,
       0,
       0,
@@ -41,6 +42,49 @@ ngx_module_t  ngx_so_module = {
     NGX_MODULE_V1_PADDING
 };
 
+static int ngx_so_check_abi(ngx_conf_t *cf, void *handle){
+    int *addon_nginx_version;
+
+    addon_nginx_version = dlsym(handle, "_ngx_so_nginx_version");
+    if(!addon_nginx_version){
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "dlsym error: %s", dlerror());
+        return NGX_ERROR;
+    }
+
+    if(nginx_version != *addon_nginx_version){
+        ngx_conf_log_error(NGX_LOG_ERR, cf, 0,
+                           "nginx version mismatch! "
+                           "working nginx version is %d "
+                           "but module was compiled with nginx version %d "
+                           "you need recompile this module.",
+                           nginx_version, *addon_nginx_version);
+        return NGX_ERROR;
+    }
+    return NGX_OK;
+}
+
+static ngx_module_t *ngx_so_get_module(ngx_conf_t *cf, void *handle){
+    ngx_module_t *module;
+    char **ngx_so_addon_name;
+
+    ngx_so_addon_name = (char **)dlsym(handle, "_ngx_so_addon_name");
+    if(!ngx_so_addon_name){
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "dlsym error: %s", dlerror());
+        return NULL;
+    }
+
+    module = dlsym(handle, *ngx_so_addon_name);
+    if(!module){
+        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
+                           "dlsym error: %s", dlerror());
+        return NULL;
+    }
+
+    return module;
+}
+
 static int so_num = 0;
 static int so_max = 16; // see auto/modules and objs/ngx_modules.c
 
@@ -50,21 +94,27 @@ ngx_so(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     void *handle;
     ngx_module_t *module;
     ngx_str_t *value = cf->args->elts;
-    char *symname = (char *)value[1].data;
-    char *filename = (char *)value[2].data;
+    char *filename = (char *)value[1].data;
     int i;
 
-    ngx_conf_log_error(NGX_LOG_STDERR, cf, 0, "loading %s", filename);
+    ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "loading %s", filename);
     handle = dlopen(filename, RTLD_LAZY);
     if(!handle){
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
                            "dlopen error: %s", dlerror());
         return NGX_CONF_ERROR;
     }
-    module = dlsym(handle, symname);
+
+    if(ngx_so_check_abi(cf, handle) != NGX_OK){
+        ngx_conf_log_error(NGX_LOG_ERR, cf, 0,
+                           "abi version missmatch %s", filename);
+        return NGX_CONF_ERROR;
+    }
+
+    module = ngx_so_get_module(cf, handle);
     if(!module){
-        ngx_conf_log_error(NGX_LOG_EMERG, cf, 0,
-                           "dlsym error: %s", dlerror());
+        ngx_conf_log_error(NGX_LOG_ERR, cf, 0,
+                           "cannot find module symbol %s", filename);
         return NGX_CONF_ERROR;
     }
 
@@ -87,6 +137,6 @@ ngx_so(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     ngx_modules[ngx_max_module++] = module;
     so_num++;
 
-    ngx_conf_log_error(NGX_LOG_STDERR, cf, 0, "loaded %s", filename);
+    ngx_conf_log_error(NGX_LOG_DEBUG, cf, 0, "loaded %s", filename);
     return NGX_CONF_OK;
 }
